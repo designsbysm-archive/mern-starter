93de26de8adf7b3eb71176593035ab7ee5fc7f28
Scott A. Matthews <scott@designsbysm.com>
2018-08-24 16:06:11 -0700

    start passport saml

--- src/server/middleware/passport.js
+++ src/server/middleware/passport.js
@@ -1,11 +1,31 @@
 const config = require('../config');
 const passport = require('passport');
 const dbModels = require('../tools/dbModels');
+const dotenv = require('dotenv');
 const LocalStrategy = require('passport-local').Strategy;
 const Model = dbModels.getModel('users');
 const passportJWT = require('passport-jwt');
 const JWTStrategy = passportJWT.Strategy;
 const ExtractJWT = passportJWT.ExtractJwt;
+const SAMLStrategy = require('passport-saml').Strategy;
+
+// load .env variables
+dotenv.config();
+const samlCert = path.resolve(process.env.SAML_CERT);
+
+passport.use(new JWTStrategy({
+        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+        secretOrKey: config.secret,
+    },
+    (jwtPayload, done) => {
+        // find the user, make sure they haven't been updated
+        Model.findOne({ username: jwtPayload.username, updatedAt: jwtPayload.updated }).then(user => {
+            return done(null, user);
+        }).catch(err => {
+            return done(err);
+        });
+    },
+));
 
 passport.use(new LocalStrategy(
     (username, password, done) => {
@@ -37,16 +57,22 @@ passport.use(new LocalStrategy(
     },
 ));
 
-passport.use(new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey: config.secret,
+passport.use(new SAMLStrategy({
+        authnContext: process.env.SAML_AUTH_CONTEXT,
+        callbackUrl: process.env.SAML_LOGIN_RESPONSE,
+        cert: samlCert,
+        entryPoint: process.env.SAML_URL,
+        identifierFormat: null,
+        issuer: process.env.SAML_LOGIN_RESPONSE,
     },
-    (jwtPayload, done) => {
-        // find the user, make sure they haven't been updated
-        Model.findOne({ username: jwtPayload.username, updatedAt: jwtPayload.updated }).then(user => {
-            return done(null, user);
-        }).catch(err => {
-            return done(err);
-        });
-    },
-));
+    (profile, done) => {
+        console.log(profile);
+
+        // findByEmail(profile.email, function(err, user) {
+        //     if (err) {
+        //         return done(err);
+        //     }
+        return done(null, 'user');
+        // });
+    }),
+);
--- src/server/routes/api/v1/saml.js
+++ src/server/routes/api/v1/saml.js
@@ -6,11 +6,43 @@ const router = require('express').Router();
 router.post('/login', (req, res, next) => {
     console.log('login');
 
+    passport.authenticate('saml', { session: false }, (err, data) => {
+        // console.log('login');
+        // if (err) {
+        //     return next(err);
+        // } else if (!data.token) {
+        //     return res.sendStatus(401);
+        // } else if (data.user.username) {
+        //     auditLog.log('authentication', {
+        //         action: 'login',
+        //         method: 'basic',
+        //         timestamp: moment().toISOString(),
+        //         username: data.user.username,
+        //     }, logHeader);
+        // }
+        //
+        // res.json({
+        //     token: data.token,
+        // });
+
+        res.sendStatus(200);
+    })(req, res, next);
+});
+
+router.post('/login/response', (req, res, next) => {
+    console.log('login responses');
+
     res.sendStatus(200);
 });
 
 router.post('/logout', (req, res, next) => {
     console.log('logout');
+
+    res.sendStatus(200);
+});
+
+router.post('/logout/responses', (req, res, next) => {
+    console.log('logout responses');
 
     res.sendStatus(200);
 });

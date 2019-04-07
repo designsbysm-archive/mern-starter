0d0a2dd75a3f6696c17809d22b1b13b420251186
Scott A. Matthews <scott@designsbysm.com>
2019-04-06 19:52:07 -0700

    lower case files/css classes

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import logo from "../images/logo.svg";
-import "../styles/App.scss";
+import "../styles/app.scss";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/app.js</code> and save to reload.
           </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
-import App from "./components/App";
+import App from "./components/app";
 // import { saveToken } from "./tools/http/appToken";
 import apiRequest from "./tools/http/apiRequest";
 

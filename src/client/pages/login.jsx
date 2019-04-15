74717c7a99d6abd141fb5dbed8a8fd70ff3f3ca7
Scott A. Matthews <scott@designsbysm.com>
2019-04-15 15:29:30 -0700

    add login form

--- src/client/pages/login.jsx
+++ src/client/pages/login.jsx
@@ -1,30 +1,64 @@
 import { apiRequest, errorHandler } from "../tools/http";
 import { saveToken } from "../tools/appToken";
-import React from "react";
+import React, { useState } from "react";
 
+//assets
+import "../styles/pages/login.scss";
+
+// TODO: grid layout (50% of screen)
 const Page = ({ loginCB }) => {
-  const options = {
-    body: {
-      password: "admin",
-      username: "admin",
-    },
-    method: "POST",
-  };
+  const [
+    username,
+    setUsername, 
+  ] = useState({});
+
+  const [
+    password,
+    setPassword, 
+  ] = useState({});
 
   return (
     <main className="contents">
-      <button
-        onClick={() =>
-          apiRequest("/api/v1/sessions/login", options)
-            .then(res => {
-              saveToken(res.token);
-              loginCB();
+      <div className="form">
+        <div className="group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            onChange={event => {
+              setUsername(event.target.value);
+            }}
+          />
+        </div>
+        <div className="group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            onChange={event => {
+              setPassword(event.target.value);
+            }}
+          />
+        </div>
+        <button
+          onClick={() => {
+            apiRequest("/api/v1/sessions/login", {
+              body: {
+                password,
+                username,
+              },
+              method: "POST",
             })
-            .catch(errorHandler)
-        }
-      >
-        Login
-      </button>
+              .then(res => {
+                saveToken(res.token);
+                loginCB();
+              })
+              .catch(errorHandler);
+          }}
+        >
+          Login
+        </button>
+      </div>
     </main>
   );
 };

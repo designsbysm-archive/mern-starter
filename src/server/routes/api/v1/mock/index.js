d096b2ba5b3738d1f0b86b4fc4ce33c3f0f89239
Scott A. Matthews <scott@designsbysm.com>
2019-04-22 12:47:32 -0700

    add /mock/sleep

--- src/server/routes/api/v1/mock/index.js
+++ src/server/routes/api/v1/mock/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import error from "./error/router";
+import sleep from "./sleep/router";
 
 const router = express.Router();
 
 router.use("/error", error);
+router.use("/sleep", sleep);
 
 export default router;
